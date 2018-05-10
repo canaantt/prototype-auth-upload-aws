3db2952a033f06aa0b5b38c0a07dcb6f59ef658d
Jenny Zhang <canaantt@users.noreply.github.com>
2018-05-09 20:30:54 -0700

    Delete ec2-initialize.sh

--- server/ec2-initialize.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-sudo -i
-docker pull canaantt/os-upload-serialization 
-docker run --name upload --env AWS_ACCESS_KEY_ID=AKIAJGNIZKZRNXV2IRDQ --env AWS_SECRET_ACCESS_KEY=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 canaantt/os-upload-serialization:latest
